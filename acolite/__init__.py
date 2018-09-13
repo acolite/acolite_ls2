f6ef33439fec23527ec7430b9364312f7214e495
Quinten Vanhellemont <quinten.vanhellemont@naturalsciences.be>
2018-09-13 08:02:53 +0200

    Added os generic path separator in relative paths

--- acolite/__init__.py
+++ acolite/__init__.py
@@ -13,19 +13,19 @@ from acolite import plotting
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 
-if not os.path.exists('{}/config'.format(path)):
+if not os.path.exists('{}{}config'.format(path, os.path.sep)):
     path = os.path.split(path)[0]
 
     ## check if binary distribution
-    if '/dist/acolite' in path:
+    if '{}dist{}acolite'.format(os.path.sep,os.path.sep) in path:
         ## two levels for this file
         for i in range(2): path = os.path.split(path)[0]
 
-cfile='{}/config/config.txt'.format(path)
+cfile='{}{}config{}config.txt'.format(path,os.path.sep,os.path.sep)
 config = import_config(cfile)
 
 ## test whether we can find the relative paths
 for t in config:
     if os.path.exists(config[t]): continue
-    tmp = path + '/' + config[t]
+    tmp = path + os.path.sep + config[t]
     config[t] = os.path.abspath(tmp)
--- acolite/acolite/__init__.py
+++ acolite/acolite/__init__.py
@@ -24,15 +24,15 @@ path=os.path.dirname(os.path.abspath(__file__))
 for i in range(2): path = os.path.split(path)[0]
 
 ## check if binary distribution
-if '/dist/acolite' in path:
+if '{}dist{}acolite'.format(os.path.sep,os.path.sep) in path:
     ## two more levels for this file
     for i in range(2): path = os.path.split(path)[0]
 
-cfile='{}/config/acolite_config.txt'.format(path)
+cfile='{}{}config{}acolite_config.txt'.format(path,os.path.sep,os.path.sep)
 config = import_config(cfile)
 
 ## test whether we can find the relative paths
 for t in config:
     if os.path.exists(config[t]): continue
-    tmp = path + '/' + config[t]
+    tmp = path + os.path.sep + config[t]
     config[t] = os.path.abspath(tmp)
