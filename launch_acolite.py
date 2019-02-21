7be3bcefc1154512d374b4b8f2a46e3baca52e5c
Quinten Vanhellemont <quinten.vanhellemont@naturalsciences.be>
2019-02-21 09:05:30 +0100

    Disabled numpy Runtime Warnings

--- launch_acolite.py
+++ launch_acolite.py
@@ -1,6 +1,7 @@
 ## wrapper to launch ACOLITE GUI/CLI
 ## QV 2018
 ## last modifications QV 2018-09-12 renamed from acolite.py and added main test
+##                    QV 2019-02-21 ignore numpy errors
 
 def launch_acolite():
     ## import sys to parse arguments
@@ -14,6 +15,10 @@ def launch_acolite():
 
     ## import acolite source
     import acolite as ac
+
+    ## ignore numpy errors
+    import numpy as np
+    olderr = np.seterr(all='ignore') 
 
     ## run command line if --cli provided, otherwise use gui
     if '--cli' in sys.argv:
