2b9a93e46749037ad6031d8f36472f057d9c0fe4
Andrea Pisani <45639805+DrMenny@users.noreply.github.com>
2019-05-03 16:08:01 +0200

    Update read_refri.py

--- acolite/shared/read_refri.py
+++ acolite/shared/read_refri.py
@@ -7,7 +7,7 @@ def read_refri():
     file = '{}/{}'.format(ac.config['pp_data_dir'], 'Shared/WOPP/computed_refri_T27_S0.dat')
 
     data = {'wave':[], 'n':[]}
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding="utf-8") as f:
         for line in f.readlines():
             if line[0] in ['%', '#', ';']: continue
             line = line.strip()
