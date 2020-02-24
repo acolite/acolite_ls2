0d184131d4a438bb0198c37fbeca18891885a1bb
Quinten Vanhellemont <quinten.vanhellemont@naturalsciences.be>
2020-02-24 07:53:03 +0100

    Re-added the l2w parameters list check

--- acolite/acolite/acolite_run.py
+++ acolite/acolite/acolite_run.py
@@ -45,6 +45,14 @@ def acolite_run(inputfile=None, output=None, limit=None, merge_tiles=None, setti
     if setu['l2w_parameters'] is not None:
         if len(setu['l2w_parameters']) == 0: setu['l2w_parameters']=None
 
+     ## making sure that the type is a list for the next step
+    if setu['l2w_parameters'] is not None and type(setu['l2w_parameters']) is not list: 
+        setu['l2w_parameters'] = [setu['l2w_parameters']]
+
+    ## removing any space between commas and the parameter name.
+    if setu['l2w_parameters'] is not None: 
+        setu['l2w_parameters'] = [par.strip() for par in setu['l2w_parameters'] if par]
+
     if setu['l2w_parameters'] is not None:
         if ('bt10' in setu['l2w_parameters']) or ('bt11' in setu['l2w_parameters']):
             setu['l8_output_bt'] = True
