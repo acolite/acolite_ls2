650331e634940fe7c9a0d8c6ff0d72855d5ae593
Quinten Vanhellemont <quinten.vanhellemont@naturalsciences.be>
2020-03-02 08:08:46 +0100

    Fixed type of number of elements in rsr convolution

--- acolite/shared/rsr_convolute_dict.py
+++ acolite/shared/rsr_convolute_dict.py
@@ -3,12 +3,13 @@
 ## written by Quinten Vanhellemont, RBINS for the PONDER project
 ## 2016-11
 ## modifications: 2017-10-17 (QV) moved wave range and step to keywords, renamed from interprsr
+##                2020-03-02 (QV) added ceil to number of elements in linspace
 
 def rsr_convolute_dict(wave_data, data, rsr, wave_range=[0.2,2.4], wave_step=0.001):
-    from numpy import linspace, interp, nan, zeros
+    from numpy import linspace, interp, nan, zeros, ceil
     
     ## set up wavelength space
-    wave_hyper = linspace(wave_range[0],wave_range[1],((wave_range[1]-wave_range[0])/wave_step)+2)
+    wave_hyper = linspace(wave_range[0],wave_range[1],ceil(((wave_range[1]-wave_range[0])/wave_step)+2))
 
     ## interpolate RSR to same dimensions
     rsr_hyper = dict()
--- acolite/shared/sensor_wave.py
+++ acolite/shared/sensor_wave.py
@@ -4,16 +4,17 @@
 ## 2018-06-07
 ## modifications: 
 ##                2018-07-18 (QV) changed acolite import name
+##                2020-03-02 (QV) added ceil to number of elements in linspace
 
 def sensor_wave(satsen, wave_range = (0.39,2.4), wave_step = 0.001):
-    from numpy import linspace
+    from numpy import linspace, ceil
     from acolite import config, rsr_convolute_dict, rsr_read
 
     pp_path = config['pp_data_dir']
     rsr_file = pp_path+'/RSR/'+satsen+'.txt'
     rsr, rsr_bands = rsr_read(file=rsr_file)
     
-    wave_hyper = linspace(wave_range[0],wave_range[1],((wave_range[1]-wave_range[0])/wave_step)+2)
+    wave_hyper = linspace(wave_range[0],wave_range[1],ceil(((wave_range[1]-wave_range[0])/wave_step)+2))
 
     rsr_wave = rsr_convolute_dict(wave_hyper, wave_hyper, rsr)
     waves = {rband:'{:.0f}'.format(round(rsr_wave[rband]*1000.)) for rband in rsr_wave}
