e40bc0ba8fd5edd76c46b83542b76f1cafd97c6b
Quinten Vanhellemont <quinten.vanhellemont@naturalsciences.be>
2020-03-10 08:39:13 +0100

    Added u/p options for EarthData access

--- acolite/shared/download_file.py
+++ acolite/shared/download_file.py
@@ -3,6 +3,7 @@
 ## written by Quinten Vanhellemont, RBINS for the PONDER project
 ## 2018-03-14
 ## modifications: 2018-11-19 (QV) added verbosity option, removed the parallel download
+##                2020-04-24 (QV) earthdata login
 
 def download_file(url, file, auth=None, session=None, parallel=False, verbosity=0):
 
@@ -12,18 +13,24 @@ def download_file(url, file, auth=None, session=None, parallel=False, verbosity=
     import os
     file_path = os.path.abspath(file)
     start = time.time()
+    
+    if 'https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/' in url:
+        username = os.environ['EARTHDATA_u']
+        password = os.environ['EARTHDATA_p']
+        auth = (username, password)
 
-    r = requests.get(url, stream=True, auth=auth)
+    with requests.Session() as session:
+            r1 = session.request('get', url)
+            r = session.get(r1.url, auth=auth)
 
-    if (r.ok):
-            with open(file_path, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024): 
-                    if chunk: # filter out keep-alive new chunks
-                        f.write(chunk)
-    else:
-            if verbosity > 2: print(r.text)
-            raise Exception("File download failed")
+            if (r.ok):
+                with open(file_path, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024*1024): 
+                        if chunk: # filter out keep-alive new chunks
+                            f.write(chunk)
+            else:
+                if verbosity > 2: print(r.text)
+                raise Exception("File download failed")
 
     if verbosity > 1:
-        print("Downloaded {}, elapsed Time: {}".format(u, time.time() - start))
-
+        print("Downloaded {}, elapsed Time: {:.1f}s".format(url, time.time() - start))
