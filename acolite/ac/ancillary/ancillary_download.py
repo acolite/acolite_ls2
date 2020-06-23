600a265e23a8d930f88d207069da64ae2d2623f3
acolite <quinten.vanhellemont@naturalsciences.be>
2020-06-23 13:23:06 +0200

    New ancillary download URL

--- acolite/ac/ancillary/ancillary_download.py
+++ acolite/ac/ancillary/ancillary_download.py
@@ -7,12 +7,13 @@
 ##                2018-07-18 (QV) changed acolite import name
 ##                2018-11-19 (QV) added verbosity option, fixed the download_file script for the GUI
 ##                2020-03-10 (QV) added autoremove for files that are too small
+##                2020-06-23 (QV) changed get url
 
-def ancillary_download(date=None, ancillary_files=None, time=None, 
-                       file_types = ['TOAST','O3_AURAOMI_24h','MET_NCEP_6h','MET_NCEP_NEXT'], 
-                       local_dir = "/storage/Data/MET", download=True, override = False, verbosity=0, 
-                       get_url = "https://oceandata.sci.gsfc.nasa.gov/cgi/getfile"):
-    
+def ancillary_download(date=None, ancillary_files=None, time=None,
+                       file_types = ['TOAST','O3_AURAOMI_24h','MET_NCEP_6h','MET_NCEP_NEXT'],
+                       local_dir = "/storage/Data/MET", download=True, override = False, verbosity=0,
+                       get_url = "https://oceandata.sci.gsfc.nasa.gov/ob/getfile"):
+
     import os
     from datetime import datetime, timedelta
     import urllib.request
@@ -20,14 +21,14 @@ def ancillary_download(date=None, ancillary_files=None, time=None,
 
     if ancillary_files == None:
         ancillary_files = []
-        
+
     if date != None:
         for bf in ancillary_list(date, file_types = file_types): ancillary_files.append(bf)
-        
+
     local_files = []
     for basefile in ancillary_files:
             yjd = basefile[1:8]
-            
+
             year = yjd[0:4]
             jday = yjd[4:7]
 
