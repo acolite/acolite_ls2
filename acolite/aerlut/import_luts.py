e2a1d22b75e95ae2916f44ea2f1e95193516869a
acolite <quinten.vanhellemont@naturalsciences.be>
2020-07-23 13:41:15 +0200

    Added RSKY import to sensor LUTs

--- acolite/aerlut/import_luts.py
+++ acolite/aerlut/import_luts.py
@@ -2,10 +2,11 @@
 ## QV 2020-01-15
 ## Last modifications: 2020-07-14 (QV) added sensor option
 ##                     2020-07-14 (QV) changed sensor option to a dict per band
+##                     2020-07-22 (QV) added the rsky lut to the lut and rgi - need to update rsky azimuths
 
 def import_luts(pressures = [500, 1013, 1100],
                 base_luts = ['PONDER-LUT-201704-MOD1', 'PONDER-LUT-201704-MOD2'],
-                sensor=None):
+                sensor=None, add_rsky=False):
     import scipy.interpolate
     import numpy as np
     import acolite as ac
@@ -30,6 +31,7 @@ def import_luts(pressures = [500, 1013, 1100],
                 rsr_file = ac.config['pp_data_dir']+'/RSR/'+sensor+'.txt'
                 rsr, rsr_bands = ac.shared.rsr_read(file=rsr_file)
 
+
             ## set up lut dimensions
             if ip == 0:
                 # lut data is par, wave, azi, thv, ths, wind, tau
@@ -39,7 +41,9 @@ def import_luts(pressures = [500, 1013, 1100],
                     lut_dim += [lut_meta['wave']]
 
                 lut_dim += [lut_meta[k] for k in ['azi', 'thv', 'ths', 'tau']]
-                lut_dict[lut] = {'meta':lut_meta, 'dim':lut_dim}
+                ipd = {par:ip for ip,par in enumerate(lut_meta['par'])}
+
+                lut_dict[lut] = {'meta':lut_meta, 'dim':lut_dim, 'ipd':ipd}
 
                 if sensor is not None:
                     lut_dict[lut]['lut'] = {band:[] for band in rsr_bands}
@@ -52,15 +56,67 @@ def import_luts(pressures = [500, 1013, 1100],
 
 
         if sensor is None:
+            if add_rsky:
+                print('Adding rsky not yet supported for generic LUT')
+
             lut_dict[lut]['lut'] = np.stack(lutd)
             ## set up LUT interpolator
             lut_dict[lut]['rgi'] = scipy.interpolate.RegularGridInterpolator(lut_dict[lut]['dim'],
                                                                              lut_dict[lut]['lut'][:,:,:,:,:,:,0,:],
                                                                              bounds_error=False, fill_value=np.nan)
         else:
-            lut_dict[lut]['rgi'] = {}
+            ## make arrays
             for band in rsr_bands:
                 lut_dict[lut]['lut'][band] = np.stack(lut_dict[lut]['lut'][band])
+
+            ## add rsky if requested
+            if add_rsky:
+                print('Update of rsky azimuths needed!')
+                rlut, rmeta, rdim, rrgi = ac.aerlut.rsky_read_lut(int(lut[-1]), sensor=sensor)
+
+                ## current pars
+                ipd = {p:i for i,p in enumerate(lut_dict[lut]['meta']['par'])}
+
+                ## run through bands
+                for band in rlut:
+                    ## add 180 degree azimuth (==0 degree) to end of rsky lut
+                    tmp = np.insert(rlut[band], (-1), rlut[band][ 0, :, :, :], axis=0)
+                    ## and remove last two vza  of rsky lut
+                    tmp = tmp[:,:-2, :,:]
+                    ## add empty axis
+                    tmp = tmp[:,:,:,None, :]
+
+                    ## add to the LUT
+                    ## model rsky at surface
+                    ## rsky_s idx 22
+                    tlut = np.insert(lut_dict[lut]['lut'][band], (22), tmp, axis=1)
+
+                    ## model rsky at toa
+                    ## rsky_t idx 23
+                    ## (utott * dtott * rsky) / (1. - rsky * astot)
+                    tmp = (tlut[:, ipd['utott'],:,:,:,:,:]*\
+                           tlut[:, ipd['dtott'],:,:,:,:,:]*
+                           tlut[:, 22,:,:,:,:,:]) /\
+                          (1.-tlut[:, 22,:,:,:,:,:] *\
+                           tlut[:, ipd['astot'],:,:,:,:,:])
+                    tlut = np.insert(tlut, (23), tmp, axis=1)
+
+                    ## add romix+rsky
+                    ## idx 24
+                    tmp = tlut[:, ipd['romix'],:,:,:,:,:] + tlut[:, 23,:,:,:,:,:]
+                    tlut = np.insert(tlut, (24), tmp, axis=1)
+
+                    ## replace in dict
+                    lut_dict[lut]['lut'][band] = tlut
+
+                ## add new pars
+                lut_dict[lut]['meta']['par'] += ['rsky_s', 'rsky_t', 'romix+rsky_t']
+                lut_dict[lut]['dim'][1]+= [22, 23, 24]
+                ## end add rsky
+
+            lut_dict[lut]['rgi'] = {}
+            lut_dict[lut]['ipd'] = {p:i for i,p in enumerate(lut_dict[lut]['meta']['par'])}
+            for band in rsr_bands:
                 ## set up LUT interpolator per band
                 lut_dict[lut]['rgi'][band] = scipy.interpolate.RegularGridInterpolator(lut_dict[lut]['dim'],
                                                                                        lut_dict[lut]['lut'][band][:,:,:,:,:,0,:],
